1b28717467be4475c559c53671ba193fb7a97cac
Andrew Leonard <ajyey@andrewleonard.dev>
2022-10-14 18:20:30 -0400

    Updates getUtil

--- src/util/getUtil.ts
+++ src/util/getUtil.ts
@@ -4,22 +4,24 @@ import { MaskedEmail, MaskedEmailState } from '../types/MaskedEmail';
  * Gets a masked email by email address from the full list of masked emails
  * @param address The email address to find from the list of masked emails
  * @param list The list of masked emails
+ * @returns The masked email if found, otherwise an empty list
  */
 export const getEmailByAddress = async (
   address: string,
   list: readonly MaskedEmail[]
-): Promise<MaskedEmail | undefined> => {
-  return list.find((me) => me.email === address);
+): Promise<MaskedEmail[]> => {
+  return list.filter((me) => me.email === address);
 };
 
 /**
  * Filters emails by state
  * @param state The state to filter by
  * @param list The list of masked emails
+ * @returns The masked emails that match the state
  */
 export const filterEmailsByState = async (
   state: MaskedEmailState,
   list: MaskedEmail[]
-) => {
+): Promise<MaskedEmail[]> => {
   return list.filter((me) => me.state === state);
 };
