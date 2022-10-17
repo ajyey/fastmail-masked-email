3fcad5716e9e7e6882415223c4237dc002b73686
Andrew Leonard <ajyey@andrewleonard.dev>
2022-10-17 14:58:15 -0400

    Adds docs, changelog, updates package.json scripts
    Adds the following deps:
    
    - gh-pages
    - open-cli
    - typedoc
    - npm-run-all

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export * from './lib/create';
 export * from './lib/get';
 export * from './lib/session';
 export * from './lib/set';
+export * from './types/MaskedEmail';
+export * from './types/MethodResponse';
+export * from './types/Options';
+export * from './types/Response';
