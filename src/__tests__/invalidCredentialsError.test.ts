30646bdce40aa74fd391a867abfabf8be2e2c90f
Andrew Leonard <ajyey@andrewleonard.dev>
2023-05-18 17:07:58 -0400

    test: adds unit tests for errors, create and session util

---
+++ src/__tests__/invalidCredentialsError.test.ts
@@ -0,0 +1,19 @@
+import { InvalidCredentialsError } from '../error/invalidCredentialsError';
+
+describe('InvalidCredentialsError', () => {
+  it('should create an instance of InvalidCredentialsError with the given message', () => {
+    const message = 'Invalid credentials';
+    const error = new InvalidCredentialsError(message);
+
+    expect(error).toBeInstanceOf(InvalidCredentialsError);
+    expect(error).toBeInstanceOf(Error);
+    expect(error.name).toBe('InvalidCredentialsError');
+    expect(error.message).toBe(message);
+  });
+
+  it('should inherit from Error', () => {
+    const error = new InvalidCredentialsError('Some message');
+
+    expect(Object.getPrototypeOf(error)).toBeInstanceOf(Error);
+  });
+});
